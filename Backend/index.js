77766124093d8e7c236634acdfb6ceee43f413fc
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-08 17:21:59 +0100

    backend development

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-
+import cookieParser from 'cookie-parser';
 import cors from 'cors'
 
 import connectDB from './config/db.js';
@@ -17,6 +17,7 @@ const app= express();
 app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 app.get('/', (req,res)=>{ 
---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,3 @@
+import jwt from "jsonwebtoken";
+
+// verify the token and decode payload information
