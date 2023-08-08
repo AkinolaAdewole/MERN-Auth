e008a7d7a7158521e38112d15f7b085f400e58a6
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-08 17:38:06 +0100

    backend development

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,3 +1,21 @@
 import jwt from "jsonwebtoken";
+import asyncHandler from 'express-async-handler';
+import userModel from "../models/userModel.js";
 
 // verify the token and decode payload information
+const protect = asyncHandler(async(req, res, next)=>{
+    let token;
+    token = req.cookies.jwt;
+
+    if(token){
+        try {
+            
+        } catch (error) {
+             res.status(401);
+             throw new Error ('Not authorized, invalid token')
+        }
+    }else{
+        res.status(401);
+        throw new Error ('Not authorized')
+    }
+})
