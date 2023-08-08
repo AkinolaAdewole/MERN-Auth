52393bac3597acbd060e2c458dd560ccd3d49238
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-08 18:13:50 +0100

    backend development

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -9,7 +9,9 @@ const protect = asyncHandler(async(req, res, next)=>{
 
     if(token){
         try {
-            
+            const decoded= jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await userModel.findById(decoded.userId).select('-password');
+            next();
         } catch (error) {
              res.status(401);
              throw new Error ('Not authorized, invalid token')
