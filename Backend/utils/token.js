c8645420fd34b29d0eec0ece4fb6464a7b46dd0c
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-08 16:06:06 +0100

    backend development API

--- Backend/utils/token.js
+++ Backend/utils/token.js
@@ -1,5 +1,11 @@
 import  jwt  from "jsonwebtoken";
 
 const generateToken=(res, userId)=>{
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET,{expiresIn:'2h'})
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET,
+        {expiresIn:'2h'});
+
+    res.cookie('jwt', token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development'
+    })
 }
