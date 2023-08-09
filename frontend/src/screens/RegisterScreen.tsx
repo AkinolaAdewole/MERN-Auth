3f1d393d83a4d6d41097ca47720430bb9689a85a
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-09 12:37:55 +0100

    SignUp and SignIn

--- frontend/src/screens/RegisterScreen.tsx
+++ frontend/src/screens/RegisterScreen.tsx
@@ -39,6 +39,37 @@ const RegisterScreen = () => {
                     onChange={(e)=>setLastName(e.target.value)}
                     ></Form.Control>
             </Form.Group>
+
+            <Form.Group className='my-2' controlId='email'>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                    type='email'
+                    placeholder='Enter email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+
+           <Form.Group className='my-2' controlId='password'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type='password'
+                    placeholder='Enter password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                ></Form.Control>
+           </Form.Group>
+
+           <Form.Group className='my-2' controlId='confirmPassword'>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+                type='password'
+                placeholder='Confirm password'
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+        
         </Form>
     </FormContainer>
   )
