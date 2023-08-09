b9e2dac2be98faaf6130b829d18c2e6cde72a2b3
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-09 12:33:56 +0100

    SignUp and SignIn

--- frontend/src/screens/RegisterScreen.tsx
+++ frontend/src/screens/RegisterScreen.tsx
@@ -5,12 +5,42 @@ import FormContainer from '../components/FormContainer';
 
 const RegisterScreen = () => {
   const [firstname, setFirstName]= useState('');
+  const [lastname, setLastName]= useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
+  const submitHandler=async(e:any)=>{
+    e.preventDefault();
+  }
+
   return (
-    <div>RegisterScreen</div>
+    <FormContainer>
+        <h1> Register</h1>
+
+        <Form onSubmit={submitHandler}>
+
+            <Form.Group className='my-2' controlId='firstname'>
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type='firstname'
+                  placeholder='Enter firstname'
+                  value={firstname}
+                  onChange={(e)=>setFirstName(e.target.value)}
+                  ></Form.Control>
+            </Form.Group>
+
+            <Form.Group className='my-2' controlId='lastname'>
+                <Form.Label>Name</Form.Label>
+                 <Form.Control
+                     type='lastname'
+                      placeholder='Enter lastname'
+                     value={lastname}
+                    onChange={(e)=>setLastName(e.target.value)}
+                    ></Form.Control>
+            </Form.Group>
+        </Form>
+    </FormContainer>
   )
 }
 
