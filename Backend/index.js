fad15475e846449a205002975f8d77312107e64f
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-10 11:37:21 +0100

    Fullstack is ready

--- Backend/index.js
+++ Backend/index.js
@@ -20,12 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use(cookieParser());
 
-app.use('/api/users', userRoutes);
-app.use(notFound);
-app.use(errorHandler);
 app.get('/', (req,res)=>{ 
     res.send('server is ready')
 });
+app.use('/api/users', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
 
 
 // if(process.env.NODE_ENV === 'production'){
